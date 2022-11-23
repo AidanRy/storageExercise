55dffcdb2ffe0de9d60d8ff3f5ea12c8332596b8
Aidan <g00365056@atu.ie>
2022-11-23 15:15:31 +0000

    included append to the file writer to stop data being erased

--- src/main/java/ie/atu/storageExercise/StorageExercise.java
+++ src/main/java/ie/atu/storageExercise/StorageExercise.java
@@ -1,10 +1,21 @@
 package ie.atu.storageExercise;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class StorageExercise {
     public static void main(String[] args) {
         File myFile = new File("MyFile.txt");
         System.out.println("My file is located at "+myFile.getAbsolutePath());
+        String content = "Details to write";
+        try{
+            FileWriter myWriter = new FileWriter(myFile,true);
+            myWriter.write("new data being written");
+            myWriter.close();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
