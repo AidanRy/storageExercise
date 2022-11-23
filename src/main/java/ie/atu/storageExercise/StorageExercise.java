12635f98df21a8ce9e951bdda67d1debdbb4bef0
Aidan <g00365056@atu.ie>
2022-11-23 15:41:33 +0000

    added a second branch and used the print writer function

--- src/main/java/ie/atu/storageExercise/StorageExercise.java
+++ src/main/java/ie/atu/storageExercise/StorageExercise.java
@@ -1,4 +1,26 @@
 package ie.atu.storageExercise;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.*;
+
+
 public class StorageExercise {
+    public static void main(String[] args) {
+        File myFile = new File("MyFile1.txt");
+        System.out.println("My file is located at "+myFile.getAbsolutePath());
+        String content = "Details to write";
+        try{
+            PrintWriter outputFile = new PrintWriter("MyFile1.txt");
+            outputFile.println("chris");
+            outputFile.println("Katheryrn");
+            outputFile.println("Jean");
+            outputFile.close();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
