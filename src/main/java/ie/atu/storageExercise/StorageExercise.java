4b3990922415f9cd1efb9de22857841941bcc81a
Aidan <g00365056@atu.ie>
2022-11-23 16:06:16 +0000

    added third branch and instnces of scanner and file input

--- src/main/java/ie/atu/storageExercise/StorageExercise.java
+++ src/main/java/ie/atu/storageExercise/StorageExercise.java
@@ -1,4 +1,20 @@
 package ie.atu.storageExercise;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class StorageExercise {
+    public static void main(String[] args) {
+        try {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("enter the file name : ");
+            String filename = keyboard.nextLine();
+            Scanner inputFile = new Scanner(new File(filename));
+            String str = inputFile.nextLine();
+            inputFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
