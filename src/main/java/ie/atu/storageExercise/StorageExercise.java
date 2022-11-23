2367c81bc2b652e79225914a290eb7b2e27db265
Aidan <g00365056@atu.ie>
2022-11-23 14:45:21 +0000

    created branch and first part of exercise.

--- src/main/java/ie/atu/storageExercise/StorageExercise.java
+++ src/main/java/ie/atu/storageExercise/StorageExercise.java
@@ -1,4 +1,10 @@
 package ie.atu.storageExercise;
 
+import java.io.File;
+
 public class StorageExercise {
+    public static void main(String[] args) {
+        File myFile = new File("MyFile.txt");
+        System.out.println("My file is located at "+myFile.getAbsolutePath());
+    }
 }
